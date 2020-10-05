071e5bb465c206c6ec3b6ce426600a201292e8b8
Ahmed <elloumiahmed55@gmail.com>
2020-10-05 10:23:55 +0100

    first-commit

---
+++ src/App.js
@@ -0,0 +1,208 @@
+import React from "react";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <nav className="navbar navbar-expand-lg navbar-light bg-light">
+        <a className="navbar-brand" href="#">
+          Navbar
+        </a>
+        <button
+          className="navbar-toggler"
+          type="button"
+          data-toggle="collapse"
+          data-target="#navbarSupportedContent"
+          aria-controls="navbarSupportedContent"
+          aria-expanded="false"
+          aria-label="Toggle navigation"
+        >
+          <span className="navbar-toggler-icon"></span>
+        </button>
+
+        <div className="collapse navbar-collapse" id="navbarSupportedContent">
+          <ul className="navbar-nav mr-auto">
+            <li className="nav-item active">
+              <a className="nav-link" href="#">
+                Home <span className="sr-only">(current)</span>
+              </a>
+            </li>
+            <li className="nav-item">
+              <a className="nav-link" href="#">
+                Link
+              </a>
+            </li>
+            <li className="nav-item dropdown">
+              <a
+                className="nav-link dropdown-toggle"
+                href="#"
+                id="navbarDropdown"
+                role="button"
+                data-toggle="dropdown"
+                aria-haspopup="true"
+                aria-expanded="false"
+              >
+                Dropdown
+              </a>
+              <div className="dropdown-menu" aria-labelledby="navbarDropdown">
+                <a className="dropdown-item" href="#">
+                  Action
+                </a>
+                <a className="dropdown-item" href="#">
+                  Another action
+                </a>
+                <div className="dropdown-divider"></div>
+                <a className="dropdown-item" href="#">
+                  Something else here
+                </a>
+              </div>
+            </li>
+            <li className="nav-item">
+              <a
+                className="nav-link disabled"
+                href="#"
+                tabIndex="-1"
+                aria-disabled="true"
+              >
+                Disabled
+              </a>
+            </li>
+          </ul>
+
+          <form className="form-inline my-2 my-lg-0">
+            <input
+              className="form-control mr-sm-2"
+              type="search"
+              placeholder="Search"
+              aria-label="Search"
+            />
+            <button
+              className="btn btn-outline-success my-2 my-sm-0"
+              type="submit"
+            >
+              Search
+            </button>
+          </form>
+        </div>
+      </nav>
+      <nav className="navbar navbar-dark bg-dark"></nav>
+
+      <nav className="navbar navbar-dark bg-primary"></nav>
+
+      <nav
+        className="navbar navbar-light"
+        style={{ backgroundColor: "#e3f2fd" }}
+      ></nav>
+
+      <div
+        id="carouselExampleInterval"
+        className="carousel slide"
+        data-ride="carousel"
+      >
+        <div className="carousel-inner">
+          <div className="carousel-item active" data-interval="10000">
+            <img src="1.jpg" className="d-block w-100" alt="..." />
+          </div>
+          <div className="carousel-item" data-interval="2000">
+            <img src="2.jpg" className="d-block w-100" alt="..." />
+          </div>
+          <div className="carousel-item">
+            <img src="3.jpg" className="d-block w-100" alt="..." />
+          </div>
+        </div>
+        <a
+          className="carousel-control-prev"
+          href="#carouselExampleInterval"
+          role="button"
+          data-slide="prev"
+        >
+          <span
+            className="carousel-control-prev-icon"
+            aria-hidden="true"
+          ></span>
+          <span className="sr-only">Previous</span>
+        </a>
+        <a
+          className="carousel-control-next"
+          href="#carouselExampleInterval"
+          role="button"
+          data-slide="next"
+        >
+          <span
+            className="carousel-control-next-icon"
+            aria-hidden="true"
+          ></span>
+          <span className="sr-only">Next</span>
+        </a>
+      </div>
+      <form>
+        <div className="form-row">
+          <div className="form-group col-md-6">
+            <label htmlFor="inputEmail4">Email</label>
+            <input type="email" className="form-control" id="inputEmail4" />
+          </div>
+          <div className="form-group col-md-6">
+            <label htmlFor="inputPassword4">Password</label>
+            <input
+              type="password"
+              className="form-control"
+              id="inputPassword4"
+            />
+          </div>
+        </div>
+        <div className="form-group">
+          <label htmlFor="inputAddress">Address</label>
+          <input
+            type="text"
+            className="form-control"
+            id="inputAddress"
+            placeholder="1234 Main St"
+          />
+        </div>
+        <div className="form-group">
+          <label htmlFor="inputAddress2">Address 2</label>
+          <input
+            type="text"
+            className="form-control"
+            id="inputAddress2"
+            placeholder="Apartment, studio, or floor"
+          />
+        </div>
+        <div className="form-row">
+          <div className="form-group col-md-6">
+            <label htmlFor="inputCity">City</label>
+            <input type="text" className="form-control" id="inputCity" />
+          </div>
+          <div className="form-group col-md-4">
+            <label htmlFor="inputState">State</label>
+            <select id="inputState" className="form-control">
+              <option defaultValue>Choose...</option>
+              <option>...</option>
+            </select>
+          </div>
+          <div className="form-group col-md-2">
+            <label htmlFor="inputZip">Zip</label>
+            <input type="text" className="form-control" id="inputZip" />
+          </div>
+        </div>
+        <div className="form-group">
+          <div className="form-check">
+            <input
+              className="form-check-input"
+              type="checkbox"
+              id="gridCheck"
+            />
+            <label className="form-check-label" htmlFor="gridCheck">
+              Check me out
+            </label>
+          </div>
+        </div>
+        <button type="submit" className="btn btn-primary">
+          Sign in
+        </button>
+      </form>
+    </div>
+  );
+}
+
+export default App;
